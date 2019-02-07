cd8b303a05eeb22b00478f22eeca94660b458485
Aaron Staggs <aaronstaggs@Aarons-MacBook-Air.local>
2019-02-07 12:39:07 -0600

    Changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import './resources/styles.css';
+
+import Header from './Components/Header_footer/Header';
+import { Toolbar } from '@material-ui/core';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        hello
+        <Header/>
+        <Toolbar/>
       </div>
     );
   }
